0a5bdbb79edebe433aa5ebc72542126d9992d802
aysu0 <c1008080@hallam.shu.ac.uk>
2024-02-23 17:16:33 +0000

    Create and Update Product Quantity, Delete Cart Items

--- cart/cart.py
+++ cart/cart.py
@@ -15,14 +15,16 @@ class Cart():
         #make sure cqart is available on all pages of site
         self.cart = cart 
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         #logic 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -36,3 +38,28 @@ class Cart():
         products = Product.objects.filter(id__in=product_ids)
         #return those looked up products
         return products
+    
+    def get_quants(self):
+        quantites = self.cart
+        return quantites 
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        #get cart
+        ourcart = self.cart
+        #update dictionary/cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+        thing = self.cart
+        return thing
+    
+    def delete(self, product):
+        product_id = str(product)
+        #delete from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+            
+        self.session.modified = True 
